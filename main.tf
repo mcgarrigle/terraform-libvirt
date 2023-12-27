2984c7859e1e519a0cb1232fd337935ed70ce804
pete <pete@smol.mac.wales>
2023-12-27 16:16:42 +0000

    variables

--- main.tf
+++ main.tf
@@ -10,30 +10,23 @@ provider "libvirt" {
   uri = "qemu:///system"
 }
 
-data "template_file" "user_data" {
-  template = file("${path.module}/cloud-init/user-data")
-}
-
-data "template_file" "network_config" {
-  template = file("${path.module}/cloud-init/network-config")
-}
-
-resource "libvirt_cloudinit_disk" "terraform_test" {
-  name           = "terraform_test.iso"
+resource "libvirt_cloudinit_disk" "cloudinit_disk" {
+  name           = "${var.guest_name}.iso"
   pool           = "filesystems"
-  user_data      = data.template_file.user_data.rendered
-  network_config = data.template_file.network_config.rendered
+  user_data      = templatefile("${path.module}/cloud-init/user-data", {})
+  meta_data      = templatefile("${path.module}/cloud-init/meta-data", { hostname = "${var.guest_host_name}" })
+  network_config = templatefile("${path.module}/cloud-init/network-config", {})
 }
 
-resource "libvirt_volume" "terraform_test" {
-  name   = "terraform_test.qcow2"
+resource "libvirt_volume" "primary_disk" {
+  name   = "${var.guest_name}.qcow2"
   pool   = "filesystems"
   source = "http://node1.mac.wales:8081/repository/cloud-images/rocky/Rocky-9-GenericCloud-Base-9.2-20230513.0.x86_64.qcow2"
   # source = "http://ftp3.br.freebsd.org/pub/rocky/9.2/images/x86_64/Rocky-9-GenericCloud-Base-9.2-20230513.0.x86_64.qcow2"
 }
 
-resource "libvirt_domain" "terraform_test" {
-  name = "terraform_test"
+resource "libvirt_domain" "guest_domain" {
+  name = var.guest_name
 
   cpu {
     mode = "host-passthrough"
@@ -42,10 +35,10 @@ resource "libvirt_domain" "terraform_test" {
   memory = 4096
 
   disk {
-    volume_id = libvirt_volume.terraform_test.id
+    volume_id = libvirt_volume.primary_disk.id
   }
 
-  cloudinit = libvirt_cloudinit_disk.terraform_test.id
+  cloudinit = libvirt_cloudinit_disk.cloudinit_disk.id
 
   network_interface {
     hostname       = "master"
---
+++ variables.tf
@@ -0,0 +1,9 @@
+variable "guest_name" {
+  type    = string
+  default = "terraform_test"
+}
+
+variable "guest_host_name" {
+  type    = string
+  default = "terraform"
+}
