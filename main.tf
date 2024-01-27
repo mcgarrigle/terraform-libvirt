10a44acf202453aa57c7ad4bdd75ec429a833305
pete <pete@smol.mac.wales>
2024-01-27 13:41:40 +0000

    moved most cloud-init config to module

--- main.tf
+++ main.tf
@@ -1,17 +1,19 @@
+
 module "libvirt_domain_foo" {
   source = "./modules/terraform-module-libvirt-domain"
 
-  guest_name = "foo"
-  cloud_init_user_data      = templatefile("${path.module}/cloud-init/user-data", {})
-  cloud_init_meta_data      = templatefile("${path.module}/cloud-init/meta-data", { hostname = "foo" })
-  cloud_init_network_config = templatefile("${path.module}/cloud-init/network-config-dhcp", {})
+  guest_name           = "foo"
+  cloud_init_user_data = file("${path.module}/cloud-init/user-data")
 }
 
 module "libvirt_domain_bar" {
   source = "./modules/terraform-module-libvirt-domain"
 
-  guest_name = "bar"
-  cloud_init_user_data      = templatefile("${path.module}/cloud-init/user-data", {})
-  cloud_init_meta_data      = templatefile("${path.module}/cloud-init/meta-data", { hostname = "bar" })
-  cloud_init_network_config = templatefile("${path.module}/cloud-init/network-config-dhcp", {})
+  guest_name           = "bar"
+  network_name         = "bridge"
+  subnet_type          = "static"
+  ip_address           = "192.168.1.24" 
+  gateway_address      = "192.168.1.254"
+  dns_server           = "192.168.1.254"
+  cloud_init_user_data = file("${path.module}/cloud-init/user-data")
 }
