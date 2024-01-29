a8dfa71b42199759a4150aff1e2abce829248271
pete <pete@smol.mac.wales>
2024-01-29 07:41:48 +0000

    support for_each

--- main.tf
+++ main.tf
@@ -1,4 +1,14 @@
 
+terraform {
+  required_providers {
+    libvirt = { source = "dmacvicar/libvirt" }
+  }
+}
+
+provider "libvirt" {
+  uri = "qemu:///system"
+}
+
 module "libvirt_domain_foo" {
   source = "./modules/terraform-module-libvirt-domain"
 
@@ -8,17 +18,21 @@ module "libvirt_domain_foo" {
   cloud_init_user_data = file("${path.module}/cloud-init/user-data")
 }
 
-module "libvirt_domain_bar" {
+module "libvirt_domain" {
   source = "./modules/terraform-module-libvirt-domain"
 
-  guest_name           = "bar"
+  providers = { libvirt = libvirt }
+
+  for_each = var.swarm
+
+  guest_name           = "${each.key}"
   vcpu                 = 2
   memory               = 4096
   network_name         = "bridge"
   subnet_type          = "static"
-  ip_address           = "192.168.1.24" 
+  ip_address           = "${each.value.ip_address}"
   gateway_address      = "192.168.1.254"
-  dns_server           = "192.168.1.254"
+  dns_server           = "1.1.1.1"
   base_volume_name     = "rocky-base-9.2"
   base_volume_size     = 20 * 1073741824
   cloud_init_user_data = file("${path.module}/cloud-init/user-data")
--- variables.tf
+++ variables.tf
@@ -1,17 +1,10 @@
-variable "image_url" {
-  type    = string
-  default = "http://node1.mac.wales:8081/repository/cloud-images/rocky/Rocky-9-GenericCloud-Base-9.2-20230513.0.x86_64.qcow2"
-  # default = "http://ftp3.br.freebsd.org/pub/rocky/9.2/images/x86_64/Rocky-9-GenericCloud-Base-9.2-20230513.0.x86_64.qcow2"
-}
 
-variable "guest_name" {
-  type    = string
-  default = "terraform_test"
-}
-
-variable "guest_host_name" {
-  type    = string
-  default = "terraform"
+variable "swarm" {
+  type = map(any)
+  default = {
+    bar1 = { ip_address = "192.168.1.24" }
+    bar2 = { ip_address = "192.168.1.25" }
+  }
 }
 
 variable "storage_pool" {
