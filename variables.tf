503cf8eb607e0fccb0f8fd85ad0fcd43ed13ba25
pete <pete@smol.mac.wales>
2024-02-02 15:45:06 +0000

    tidy

--- variables.tf
+++ variables.tf
@@ -3,17 +3,17 @@ variable "cluster" {
   type = map(any)
   default = {
     bar1 = {
-             ip_address       = "192.168.1.24" 
-             vcpu             = 2
-             memory           = 4096
-             base_volume_size = 20 * 1073741824
-           }
+      ip_address       = "192.168.1.24" 
+      vcpu             = 2
+      memory           = 4096
+      base_volume_size = 20 * 1073741824
+    }
     bar2 = {
-            ip_address        = "192.168.1.25" 
-             vcpu             = 2
-             memory           = 4096
-             base_volume_size = 20 * 1073741824
-           }
+      ip_address        = "192.168.1.25" 
+      vcpu             = 2
+      memory           = 4096
+      base_volume_size = 20 * 1073741824
+    }
   }
 }
 
